5b6af36b0f6daa9d34b3506e7b3b62c4ad76ad58
baraah <baraah.alshiaani@gmail.com>
2026-01-28 19:58:55 +0100

    first commit

--- task-1/book.js
+++ task-1/book.js
@@ -1,3 +1,17 @@
 function isBookApplicable(searchString) {
-  // Your code here
+  const bookTitle ="The fundamentals of JavaScript";
+
+  if (!searchString) 
+    return false;
+
+  const normalSearch = searchString.trim().toLowerCase();
+  const normalTitle = bookTitle.toLowerCase();
+
+  return normalTitle.includes(normalSearch);
 }
+
+console.log(isBookApplicable("javascript"));
+console.log(isBookApplicable("javascript "));
+console.log(isBookApplicable("python"));
+console.log(isBookApplicable("JavaScript"));
+console.log(isBookApplicable("JAVASCRIPT"));
